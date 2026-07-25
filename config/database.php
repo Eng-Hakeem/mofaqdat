f6171c5f4f84ea397444b64b239b501f8078f433
Eng-Hakeem <24164442@su.edu.ye>
2026-07-25 20:12:59 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ define('MAX_UPLOAD_SIZE', 5 * 1024 * 1024);
 
 try {
     $pdo = new PDO(
-        "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=utf8mb4",
+        "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=utf8mb4"
         DB_USER,
         DB_PASS,
         [
